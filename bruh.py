aec65319a4b7fbab1c6623deef5d6ee06a8276e8
ardhan <ardhan.s.pratama@gmail.com>
2022-03-15 20:57:44 +0800

    Added dictionary to store values

--- bruh.py
+++ bruh.py
@@ -2,6 +2,25 @@ import discord
 
 client = discord.Client()
 
+# dictionary which contains usernames and corresponding amount of bruh moments
+userDict = {}
+
+def printLog(message):
+    print("User:", message.author)
+    print("Message:", message.content)
+    print()
+
+# def printBotMessage(message):
+
+def addBruhMoment(message):
+    username = message.author.name + "#" + message.author.discriminator
+
+    if username in userDict:
+        counter = userDict.get(username) + 1
+        userDict[username] = counter
+    else:
+        userDict[username] = 1
+
 @client.event
 async def on_ready():
     print("Bot initialised...")
@@ -12,8 +31,10 @@ async def on_message(message):
         return
 
     if "bruh" in message.content:
-        print(message.author)
-        print(message)
-        await message.channel.send("bruh")
+        printLog(message)
+        addBruhMoment(message)
+        print(userDict)
+        await message.channel.send("bruh moment")
+        await message.channel.send("lol")
 
-client.run("Insert bot token here")
+client.run("token")
